27ccd37f362c9cfd467c30d703a6a848f9a3163d
eternal-f1ame <thunder.tanay.1907@gmail.com>
2022-10-08 01:00:04 +0530

    Display Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Home from './Home';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <div className="content">
+                AI Society Website
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
