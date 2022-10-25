edac4d485930253c22082185a1bc1288b87b4f66
PJiyush <jhamnani.1@iitj.ac.in>
2022-10-25 20:40:34 +0530

    changed the layout for smooth functioning on small screen devices

--- src/Mycomponents/Ourteam.js
+++ src/Mycomponents/Ourteam.js
@@ -8,12 +8,12 @@ function Ourteam() {
     <Navbar/>
     <div className="color">
     <div className="grid-container">
-      <span><div className="a1" ><Cards name = 'piyush' post='ok' description='lorem ipsum'/></div></span>
-      <span><div className="a2" ><Cards  name = 'piyush'/></div>
-      <div className="a3" ><Cards name = 'piyush'/></div></span>
-      <span><div className="a4" ><Cards name = 'piyush'/></div>
-      <div className="a5" ><Cards name = 'piyush'/></div>
-      <div className="a6" ><Cards name = 'piyush'/></div></span>
+      <span><div className="space" ><Cards name = 'piyush' post='ok' description='lorem ipsum'/> <br /> </div></span>
+      <span><div className="space" ><Cards  name = 'piyush'/> <br /></div>
+      <div className="space" ><Cards name = 'piyush'/> <br /></div></span>
+      <span><div className="space" ><Cards name = 'piyush'/> <br /></div>
+      <div className="space" ><Cards name = 'piyush'/> <br /></div>
+      <div className="space" ><Cards name = 'piyush'/> <br /></div></span>
       {/* to add more we just create more div and then put card component in that div name, post, description also enter it during card component as prop */}
       {/* by wrapping up it in the span we can generate hirearchial structure */}
 
