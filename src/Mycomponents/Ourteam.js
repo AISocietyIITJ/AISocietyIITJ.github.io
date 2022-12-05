cb85009436e1f76161e8f010571b3d698d772592
PJiyush <jhamnani.1@iitj.ac.in>
2022-12-05 21:56:58 +0530

    Made the OurTeam page dynamic and improved the layout of page

--- src/Mycomponents/Ourteam.js
+++ src/Mycomponents/Ourteam.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 import Cards from './Cards'
 import './Ourteam.css'
+import Data from '../DataOurTeam'
 function Ourteam() {
+  const [people, setPeople] = useState(Data);
   return (
     <>
     <Navbar/>
     <div className="color">
     <div className="grid-container">
-      <span>
+      {/* <span>
         <div className="space" id='first' ><Cards name = 'Coordinator'/> <br /></div>
         <div className="space" ><Cards name = 'Coordinator'/> <br /></div>
         <div className="space" ><Cards name = 'Coordinator'/> <br /></div>
@@ -22,10 +24,10 @@ function Ourteam() {
         <div className="space" ><Cards name = 'Core Member'/> <br /></div>
         <div className="space" ><Cards name = 'Core Member'/> <br /></div>
         <div className="space" ><Cards name = 'Core Member'/> <br /></div>
-      </span>
+      </span> */}
       {/* to add more we just create more div and then put card component in that div name, post, description also enter it during card component as prop */}
       {/* by wrapping up it in the span we can generate hirearchial structure */}
-
+      <Cards persons = {people}/>
     </div>;  
     </div>
     </>
